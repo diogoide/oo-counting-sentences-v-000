ff8cd7b81ffa5a4787ab3ad010c79d422ce50c1e
Diogo Ide <diogoide@gmail.com>
2018-04-18 23:39:28 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,14 +15,8 @@ class String
   end
 
   def count_sentences
-    #sentences = []
-
-      #sentences <<
       sentences = self.split(/[!.,]/).reject {|c| c.empty?}
-    #  new_sentences = sentences.flatten.reject {|c| c.empty?}
-      #new_sentences.count
       sentences.count
-
       end
 
 end
